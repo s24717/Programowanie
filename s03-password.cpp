44338f97992ca6cc81581c03639458abeeb1ace1
s24717 <92875981+s24717@users.noreply.github.com>
2021-10-25 19:46:47 +0200

    Update s03-password.cpp

--- s03-password.cpp
+++ s03-password.cpp
@@ -1,18 +1,23 @@
 #include <string>
 #include <iostream>
 
-std::string haslo;
 std::string a;
-
-auto main() -> int
+std::string b;
+auto main(int argc, char* argv[]) -> int
 {
-	std::cout << "Witam na naszej stronie!!" << "\n";
-	do
-	{	
-		std::cout << "Haslo: "; std::getline(std::cin, a);
+	b = argv[1];
+	if(b=="student")
+	{
+		std::cout << "Podales poprawne ponowne haslo"<< "\n";
 	}
-	while(a!="student");
-	std::cout << "Podales poprawne ponowne haslo"<< "\n";
+	else
+	{
+		do
+		{	
+			std::cout << "Haslo: "; std::getline(std::cin, a);
+		}
+		while(a!="student");
+		std::cout << "Podales poprawne ponowne haslo"<< "\n";
+	}	
 	return 0;
 }
-		
