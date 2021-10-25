7a081b8b72f6dc90ff15541990c08f8604272074
s24717 <92875981+s24717@users.noreply.github.com>
2021-10-26 00:12:14 +0200

    Update s03-guessing-game.cpp

--- s03-guessing-game.cpp
+++ s03-guessing-game.cpp
@@ -5,24 +5,32 @@
 int jakas_randomowa_liczba;
 int liczba;
 
-int main()
+
+auto main(int argc, char* argv[]) -> int
 {	srand(time(NULL));
 	jakas_randomowa_liczba = rand()%100+1;
-	std::cout << "System wybral liczbe z przedzialu 1-100 sprobuj ja zgnadnac: " << "\n";
 	
+	
+    
 	do
-	{
-		std::cout << "quess: "; std::cin >> liczba;
+		{
+			
+			if(liczba > jakas_randomowa_liczba)
+			{
+				std::cout << "number too big!" << "\n";
+			}
+			else if(liczba < jakas_randomowa_liczba)
+			{
+				std::cout << "number too small!" << "\n";	
+			}
+			
+			std::cout << "quess: "; std::cin >> liczba;
+			
+		}while(liczba!=jakas_randomowa_liczba);
 		
-		if(liczba > jakas_randomowa_liczba)
-		{
-			std::cout << "number too big!" << "\n";
-		}
-		else if(liczba < jakas_randomowa_liczba)
-		{
-			std::cout << "number too small!" << "\n";	
-		}
-	}while(liczba!=jakas_randomowa_liczba);
-	std::cout << "just right!" << "\n";
+		std::cout << "just right!" << "\n";
+	
+		
 	return 0;
+	
 }
