22223c2a60f2d6c832896cfc117d5b05634682d8
s24717 <92875981+s24717@users.noreply.github.com>
2021-11-19 12:33:02 +0100

    Create 2.0.0.9.cpp

---
+++ 2.0.0.9.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int a;
+int iloczyn=1;
+auto main(int argc, char* argv[]) -> int
+{
+	a = std::stoi(argv[1]);
+	
+	
+	while(i<=a)
+	{
+		iloczyn=iloczyn*i;
+		i++;
+	}
+	
+	std::cout << iloczyn << "\n";
+	
+
+	return 0;
+}
