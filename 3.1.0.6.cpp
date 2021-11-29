f7d34a06139bb79d0a53010882324e113cf7b65c
s24717 <92875981+s24717@users.noreply.github.com>
2021-11-29 21:24:14 +0100

    Create 3.1.0.6.cpp

---
+++ 3.1.0.6.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+int needle;
+int liczba=0;
+int tablica[10] = {42, 9 , -1 , 18, 59, 3, 101, 31, 72, 12};
+
+void wynik(int b)
+{
+	for(int i=0;i<=9;i++)
+	{
+		if(tablica[i]==b)
+		{
+			std::cout << i << "\n";
+			break;
+		}
+		else if(i==9&&tablica[i]!=b)
+		{
+			std::cout << "-1" << "\n";
+		}	
+	}
+	
+
+}
+
+
+auto main(int argc, char* argv[]) -> int
+{
+	
+	needle = std::stoi(argv[1]);
+	wynik(needle);
+	
+	return 0;
+}
