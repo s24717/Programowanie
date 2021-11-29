fb0103e5d6ed3bc37a40d932a233d40d792060fd
s24717 <92875981+s24717@users.noreply.github.com>
2021-11-29 03:41:49 +0100

    Create 3.1.0.2.cpp

---
+++ 3.1.0.2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+int a,b;
+int tab[100];
+
+void tablica(int zmienna, int zmienna2)
+{
+    for(int i=zmienna2;i<=zmienna+zmienna2;i++)
+    {   
+        tab[i] = i;
+        std::cout << tab[i] << "\n";
+    }
+}
+
+
+auto main(int argc, char* argv[]) -> int
+{
+	a = std::stoi(argv[1]);
+    b = std::stoi(argv[2]);
+    tablica(a, b);
+
+	return 0;
+}
