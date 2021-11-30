ac35aa3c13bf0a7e8aa8041eb9ddfd0384113af7
s24717 <92875981+s24717@users.noreply.github.com>
2021-11-30 15:24:41 +0100

    Update 3.1.0.3.cpp

--- 3.1.0.3.cpp
+++ 3.1.0.3.cpp
@@ -5,7 +5,7 @@ int a=3;
 int suma=0;
 int tablica[10] = {42, 9 , -1 , 18, 59, 3, 101, 31, 72, 12};
 
-void wynik(int b)
+void asum(int b)
 {
 	for(int i=0;i<=9;i++)
 	{
@@ -20,7 +20,7 @@ auto main(int argc, char* argv[]) -> int
 {
 	
 	std::cout << "wynik sumy tablicy to:";
-	wynik(a);
+	asuma(a);
 	
 	return 0;
 }
