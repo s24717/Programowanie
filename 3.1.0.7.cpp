3a643d1feba652428b2dce09ed0a8b9bc8781f32
s24717 <92875981+s24717@users.noreply.github.com>
2021-11-30 15:19:04 +0100

    Create 3.1.0.7.cpp

---
+++ 3.1.0.7.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+int xd,a;
+
+int tablica[10] = {42, 9 , -1 , 18, 59, 3, 101, 31, 72, 12};
+
+
+void sort_asc(int b)
+{
+	for(int i=0;i<=9;i++)
+	{
+		//tutaj iteruje sie po tablicy 0 1 2 3 i tak dalej
+		
+		for(int j=0;j<=9;j++)
+		{
+			//o tak tutaj bedziemy wybrany podmiot porownywac z innymi podmiotami w tablicy
+			if(tablica[i] < tablica[j])
+			{
+				xd =  tablica[i];
+				tablica[i] = tablica[j];
+				tablica[j] = xd;
+			}
+			else
+			{
+				continue;
+			}	
+		}
+	}
+}
+
+
+auto main(int argc, char* argv[]) -> int
+{
+	
+	std::cout << "o to tablica nasza kochana: " << "\n";
+	sort_asc(a);
+	
+	
+	
+	
+	for(int k=0;k<=9;k++)
+	{
+		std::cout << tablica[k] << "\n";
+	}
+	return 0;
+}
